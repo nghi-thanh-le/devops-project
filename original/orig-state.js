af6aa83a1c9b4f0a23526affd84809cca0d36c5d
Nghi Le Thanh <nghi.le@tuni.fi>
2020-11-22 00:26:53 +0100

    Current state by json but recursive reloading

--- original/orig-state.js
+++ original/orig-state.js
@@ -1,18 +1,49 @@
 const fs = require('fs');
 const os = require('os');
 
+const isEmpty = (obj) => {
+    for (let prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            return false;
+        }
+    }
+
+    return JSON.stringify(obj) === JSON.stringify({});
+};
+
 const State = (() => {
+    const STATE_LOCATION = `${__dirname}/state.json`;
+    const DEFAULT_STATE = 'RUNNING';
     let state = '';
-    const INIT_STATE = 'INIT';
+
+    const initState = () => {
+        // because state.json is small. it's fine to sync access it. 
+        const jsonData = (() => {
+            try {
+                const jsonData = fs.readFileSync(STATE_LOCATION);
+                return JSON.parse(jsonData);
+            } catch (error) {
+                console.error(error);
+                fs.writeFile(`${__dirname}/state.json`, JSON.stringify({
+                    currentState: 'INIT'
+                }), err => {
+                    if (err) console.error(err);
+                });
+                return {};
+            }
+        })();
+
+        return isEmpty(jsonData) ? DEFAULT_STATE : jsonData.currentState;;
+    };
 
     const logState = () => {
         if (state === '') {
             console.warn('Empty state, skip log');
             return;
         }
-    
+
         const msgToWrite = `${(new Date()).toISOString()}: ${state}`;
-        // console.log(msgToWrite);
+        console.log(msgToWrite);
         fs.appendFile(`${__dirname}/log/orig-state.log`, `${msgToWrite}${os.EOL}`, { 'flag': 'a' }, (err) => {
             if (err) {
                 console.error(err);
@@ -24,9 +55,9 @@ const State = (() => {
 
     const getState = () => {
         if (state === '') {
-            state = INIT_STATE;
-            logState();
+            state = initState();
         }
+        logState();
         return state;
     };
 
@@ -37,9 +68,17 @@ const State = (() => {
         }
         state = newState;
         logState();
+
+        const newStateData = JSON.stringify({
+            currentState: state
+        });
+
+        fs.writeFile(`${__dirname}/state.json`, newStateData, 'utf-8', (err, data) => {
+            if(err) console.error(err);
+        });
     };
 
-    return {getState, setState};
+    return { getState, setState };
 })();
 
 module.exports = { State };
--- original/orig.js
+++ original/orig.js
@@ -9,29 +9,29 @@ console.log('Switch state to RUNNING');
 State.setState('RUNNING');
 console.log(`Current State: ${State.getState()}`);
 
-amqp.connect(process.env.MESSAGE_QUEUE, (error0, connection) => {
-    if (error0) {
-        throw error0;
-    }
-    console.log('[*] Connect to rabbitmq from Original')
+// amqp.connect(process.env.MESSAGE_QUEUE, (error0, connection) => {
+//     if (error0) {
+//         throw error0;
+//     }
+//     console.log('[*] Connect to rabbitmq from Original')
 
-    connection.createChannel((error1, channel) => {
-        if(error1) {
-            throw error1;
-        }
-        const EXCHANGE = 'message_queue';
-        const key = 'my.o';
-        let msgCount = 1;
+//     connection.createChannel((error1, channel) => {
+//         if(error1) {
+//             throw error1;
+//         }
+//         const EXCHANGE = 'message_queue';
+//         const key = 'my.o';
+//         let msgCount = 1;
 
-        channel.assertExchange(EXCHANGE, 'topic', {
-            durable: false
-        });
+//         channel.assertExchange(EXCHANGE, 'topic', {
+//             durable: false
+//         });
 
-        const timer = setInterval(() => {
-            const sentMsg = `MSG_${msgCount++}`;
-            channel.publish(EXCHANGE, key, Buffer.from(sentMsg));
-            console.log(" [x] Sent %s from ORIG", sentMsg);
+//         const timer = setInterval(() => {
+//             const sentMsg = `MSG_${msgCount++}`;
+//             channel.publish(EXCHANGE, key, Buffer.from(sentMsg));
+//             console.log(" [x] Sent %s from ORIG", sentMsg);
 
-        }, 100000);
-    });
-});
+//         }, 100000);
+//     });
+// });
