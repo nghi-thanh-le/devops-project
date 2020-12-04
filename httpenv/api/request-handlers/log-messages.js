b1e81cc0af3ea54732ea4430176fe2e98d38e35c
nghi.thanh <nghi.thanh@valtech.com>
2020-12-05 00:42:46 +0200

    Fix log printout issue

--- httpenv/api/request-handlers/log-messages.js
+++ httpenv/api/request-handlers/log-messages.js
@@ -1,19 +1,23 @@
 const fs = require('fs')
+const lineByLine = require('n-readlines');
 const path = require('path');
 const OBSE_LOG_LOCATION = path.resolve(`${__dirname}/../log/obse.log`);
 const ORIG_STATE_LOG_LOCATION = path.resolve(`${__dirname}/../log/orig-state.log`);
 
 const readFileThenSendBack = (fileLocation, req, res) => {
-  fs.readFile(fileLocation, 'utf-8', (e, data) => {
-    if (e) {
-      console.error(e);
-      res.status(400);
-      res.send(e);
-    } else {
-      res.status(200);
-      res.send(data);
+  if (!fs.existsSync(fileLocation)) {
+    res.status(404);
+    res.send('Log file does not exist');
+  } else {
+    res.status(200);
+    const liner = new lineByLine(fileLocation);
+    let line;
+    let data = '';
+    while (line = liner.next()) {
+      data = data.concat(`${line.toString('ascii')}<br/>`);
     }
-  });
+    res.send(data);
+  }
 }
 
 const obseLogHanlder = (req, res) => {
