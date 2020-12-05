55d4116aa114fa3ef7e1760e64d8a412c4da5b25
nghi.thanh <nghi.thanh@valtech.com>
2020-12-05 16:52:01 +0200

    Add lint for original

--- original/orig-constants.js
+++ original/orig-constants.js
@@ -1,7 +1,7 @@
 module.exports = Object.freeze({
-    STATE_UPDATE_CHANNEL: 'state-change',
-    STATE_UPDATE_TYPE: 'fanout',
-    BROADCAST_MESSAGE_CHANNEL: 'message_queue',
-    BROADCAST_MESSAGE_TOPIC: 'my.o',
-    BROADCAST_MESSAGE_TYPE: 'topic'
+  STATE_UPDATE_CHANNEL: 'state-change',
+  STATE_UPDATE_TYPE: 'fanout',
+  BROADCAST_MESSAGE_CHANNEL: 'message_queue',
+  BROADCAST_MESSAGE_TOPIC: 'my.o',
+  BROADCAST_MESSAGE_TYPE: 'topic',
 });
--- original/orig-funcs.js
+++ original/orig-funcs.js
@@ -2,60 +2,88 @@ const ORIG_CONSTANTS = require('./orig-constants.js');
 const State = require('./orig-state.js');
 
 const broadcastIntervalMessages = (channel) => {
-    let msgCount = 1;
+  let msgCount = 1;
 
-        channel.assertExchange(ORIG_CONSTANTS.BROADCAST_MESSAGE_CHANNEL, ORIG_CONSTANTS.BROADCAST_MESSAGE_TYPE, {
-            durable: false
-        });
+  channel.assertExchange(
+    ORIG_CONSTANTS.BROADCAST_MESSAGE_CHANNEL,
+    ORIG_CONSTANTS.BROADCAST_MESSAGE_TYPE,
+    {
+      durable: false,
+    }
+  );
 
-    const timer = setInterval(() => {
-        console.log('Current State: ' + State.getState());
-        if (State.getState() === State.RUNNING_STATE) {
-            const sentMsg = `MSG_${msgCount++}`;
-            channel.publish(ORIG_CONSTANTS.BROADCAST_MESSAGE_CHANNEL, ORIG_CONSTANTS.BROADCAST_MESSAGE_TOPIC, Buffer.from(sentMsg));
-            console.log(`[x] Sent ${sentMsg} from ORIG`);
-        }
-    }, 25000);
+  const timer = setInterval(() => {
+    console.log('Current State: ', State.getState());
+    if (State.getState() === State.RUNNING_STATE) {
+      const sentMsg = `MSG_${msgCount++}`;
+      channel.publish(
+        ORIG_CONSTANTS.BROADCAST_MESSAGE_CHANNEL,
+        ORIG_CONSTANTS.BROADCAST_MESSAGE_TOPIC,
+        Buffer.from(sentMsg)
+      );
+      console.log(`[x] Sent ${sentMsg} from ORIG`);
+    }
+  }, 25000);
 
-    return timer;
+  return timer;
 };
 
-const subscribeToStateUpdateChannel = (channel, connection, intervalMessages) => {
-    channel.assertExchange(ORIG_CONSTANTS.STATE_UPDATE_CHANNEL, ORIG_CONSTANTS.STATE_UPDATE_TYPE, {
-        durable: false
-    });
+const subscribeToStateUpdateChannel = (
+  channel,
+  connection,
+  intervalMessages
+) => {
+  channel.assertExchange(
+    ORIG_CONSTANTS.STATE_UPDATE_CHANNEL,
+    ORIG_CONSTANTS.STATE_UPDATE_TYPE,
+    {
+      durable: false,
+    }
+  );
 
-    channel.assertQueue('', {
-        exclusive: true
-    }, (error2, q) => {
-        if (error2) {
-            throw error2;
-        }
-        console.log(`[*] Waiting for messages in ${q.queue}`);
-        channel.bindQueue(q.queue, ORIG_CONSTANTS.STATE_UPDATE_CHANNEL, '');
+  channel.assertQueue(
+    '',
+    {
+      exclusive: true,
+    },
+    (error2, q) => {
+      if (error2) {
+        throw error2;
+      }
+      console.log(`[*] Waiting for messages in ${q.queue}`);
+      channel.bindQueue(q.queue, ORIG_CONSTANTS.STATE_UPDATE_CHANNEL, '');
 
-        channel.consume(q.queue, (msg) => {
-            if (msg.content) {
-                console.log(`[x] ${msg.content.toString()}`);
-                const newState = msg.content === undefined ? State.getState() : msg.content.toString();
-                if (newState === State.getState()) {
-                    console.log('Same state as current state, skip do nothing!');
-                } else {
-                    State.setState(newState);
-                    if (newState === State.SHUTDOWN_STATE) {
-                        console.log('shutting down');
-                        clearInterval(intervalMessages);
-                        connection.close();
-                        process.exit(0);
-                    }
-                }
+      channel.consume(
+        q.queue,
+        (msg) => {
+          if (msg.content) {
+            console.log(`[x] ${msg.content.toString()}`);
+            const newState =
+              msg.content === undefined
+                ? State.getState()
+                : msg.content.toString();
+            if (newState === State.getState()) {
+              console.log('Same state as current state, skip do nothing!');
+            } else {
+              State.setState(newState);
+              if (newState === State.SHUTDOWN_STATE) {
+                console.log('shutting down');
+                clearInterval(intervalMessages);
+                connection.close();
+                process.exit(0);
+              }
             }
-        }, {
-            noAck: true
-        });
-    });
+          }
+        },
+        {
+          noAck: true,
+        }
+      );
+    }
+  );
 };
 
 module.exports = {
-    broadcastIntervalMessages,
-    subscribeToStateUpdateChannel}
+  broadcastIntervalMessages,
+  subscribeToStateUpdateChannel,
+};
