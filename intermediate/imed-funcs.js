35d8c48de78b01ccea916bb95c1ea37e6578b006
nghi.thanh <nghi.thanh@valtech.com>
2020-12-05 16:45:00 +0200

    Add lint for intermidiate

--- intermediate/imed-funcs.js
+++ intermediate/imed-funcs.js
@@ -1,64 +1,79 @@
 const {
-    STATE_UPDATE_CHANNEL,
-    STATE_UPDATE_TYPE,
-    STATE_SHUTDOWN,
-    BROADCAST_MESSAGE_CHANNEL,
-    BROADCAST_MESSAGE_TOPIC,
-    BROADCAST_MESSAGE_TYPE
+  STATE_UPDATE_CHANNEL,
+  STATE_UPDATE_TYPE,
+  STATE_SHUTDOWN,
+  BROADCAST_MESSAGE_CHANNEL,
+  BROADCAST_MESSAGE_TOPIC,
+  BROADCAST_MESSAGE_TYPE,
 } = require('./imed-constants.js');
 
 const listenToTopicAndBroadcast = (channel) => {
-    channel.assertExchange(BROADCAST_MESSAGE_CHANNEL, 
-                           BROADCAST_MESSAGE_TYPE, 
-                           { durable: false });
+  channel.assertExchange(BROADCAST_MESSAGE_CHANNEL, BROADCAST_MESSAGE_TYPE, {
+    durable: false,
+  });
 
-    channel.assertQueue('', { exclusive: true }, (error2, q) => {
-        if (error2) throw error2;
+  channel.assertQueue('', { exclusive: true }, (error2, q) => {
+    if (error2) throw error2;
 
-        channel.bindQueue(q.queue, 
-                          BROADCAST_MESSAGE_CHANNEL, 
-                          BROADCAST_MESSAGE_TOPIC);
+    channel.bindQueue(
+      q.queue,
+      BROADCAST_MESSAGE_CHANNEL,
+      BROADCAST_MESSAGE_TOPIC
+    );
 
-        channel.consume(q.queue, msg => {
-            setTimeout(() => {
-                const msgToSend = `Got: ${msg.content.toString()}`
-                console.log(msgToSend);
-                channel.publish(BROADCAST_MESSAGE_CHANNEL, BROADCAST_MESSAGE_TOPIC, Buffer.from(msgToSend));
-            }, 1000);
-        }, { noAck: true });
-    });
+    channel.consume(
+      q.queue,
+      (msg) => {
+        setTimeout(() => {
+          const msgToSend = `Got: ${msg.content.toString()}`;
+          console.log(msgToSend);
+          channel.publish(
+            BROADCAST_MESSAGE_CHANNEL,
+            BROADCAST_MESSAGE_TOPIC,
+            Buffer.from(msgToSend)
+          );
+        }, 1000);
+      },
+      { noAck: true }
+    );
+  });
 };
 
 const subscribeToShutdownChannel = (channel, connection) => {
-    channel.assertExchange(STATE_UPDATE_CHANNEL, 
-                           STATE_UPDATE_TYPE, 
-                           { durable: false });
+  channel.assertExchange(STATE_UPDATE_CHANNEL, STATE_UPDATE_TYPE, {
+    durable: false,
+  });
 
-    channel.assertQueue('', { exclusive: true }, (error2, q) => {
-        if (error2) { 
-            throw error2;
+  channel.assertQueue('', { exclusive: true }, (error2, q) => {
+    if (error2) {
+      throw error2;
+    }
+
+    console.log(`[*] Waiting for messages in ${q.queue}`);
+    channel.bindQueue(q.queue, STATE_UPDATE_CHANNEL, '');
+
+    channel.consume(
+      q.queue,
+      (msg) => {
+        if (msg.content) {
+          console.log(`[x] ${msg.content.toString()}`);
+          const newState =
+            msg.content === undefined ? '' : msg.content.toString();
+          if (newState !== STATE_SHUTDOWN) {
+            console.log('Invalid state, skip do nothing!');
+          } else {
+            console.log('shutting down');
+            connection.close();
+            process.exit(0);
+          }
         }
-
-        console.log(`[*] Waiting for messages in ${q.queue}`);
-        channel.bindQueue(q.queue, STATE_UPDATE_CHANNEL, '');
-
-        channel.consume(q.queue, (msg) => {
-            if (msg.content) {
-                console.log(`[x] ${msg.content.toString()}`);
-                const newState = msg.content === undefined ? '' : msg.content.toString();
-                if (newState !== STATE_SHUTDOWN) {
-                    console.log('Invalid state, skip do nothing!');
-                } else {
-                    console.log('shutting down');
-                    connection.close();
-                    process.exit(0);
-                }
-            }
-        }, { noAck: true });
-    });
-}
+      },
+      { noAck: true }
+    );
+  });
+};
 
 module.exports = {
-    listenToTopicAndBroadcast,
-    subscribeToShutdownChannel
-}
+  listenToTopicAndBroadcast,
+  subscribeToShutdownChannel,
+};
--- intermediate/imed.js
+++ intermediate/imed.js
@@ -2,16 +2,16 @@ const amqp = require('amqplib/callback_api');
 const imedFunctions = require('./imed-funcs.js');
 
 amqp.connect(process.env.MESSAGE_QUEUE, (error0, connection) => {
-    if (error0) {
-        throw error0;
+  if (error0) {
+    throw error0;
+  }
+  console.log('[*] Connect to rabbitmq from Intermediate');
+  connection.createChannel((error1, channel) => {
+    if (error1) {
+      throw error1;
     }
-    console.log('[*] Connect to rabbitmq from Intermediate')
-    connection.createChannel((error1, channel) => {
-        if(error1) {
-            throw error1;
-        }
 
-        imedFunctions.listenToTopicAndBroadcast(channel);
-        imedFunctions.subscribeToShutdownChannel(channel, connection);
-    });
+    imedFunctions.listenToTopicAndBroadcast(channel);
+    imedFunctions.subscribeToShutdownChannel(channel, connection);
+  });
 });
