97fee636be68212e3a650139039ab9486174865d
nghi.thanh <nghi.thanh@valtech.com>
2020-12-05 16:47:40 +0200

    Add lint for obserever

--- observer/obse-funcs.js
+++ observer/obse-funcs.js
@@ -1,67 +1,85 @@
-const {
-    STATE_UPDATE_CHANNEL,
-    STATE_UPDATE_TYPE,
-    STATE_SHUTDOWN,
-    BROADCAST_MESSAGE_CHANNEL,
-    BROADCAST_MESSAGE_TOPIC,
-    BROADCAST_MESSAGE_TYPE
-} = require('./obse-constants.js');
 const fs = require('fs');
 const os = require('os');
+const {
+  STATE_UPDATE_CHANNEL,
+  STATE_UPDATE_TYPE,
+  STATE_SHUTDOWN,
+  BROADCAST_MESSAGE_CHANNEL,
+  BROADCAST_MESSAGE_TOPIC,
+  BROADCAST_MESSAGE_TYPE,
+} = require('./obse-constants.js');
 
 const listenToTopicAndBroadcast = (channel) => {
-    channel.assertExchange(BROADCAST_MESSAGE_CHANNEL, 
-                           BROADCAST_MESSAGE_TYPE, 
-                           { durable: false });
+  channel.assertExchange(BROADCAST_MESSAGE_CHANNEL, BROADCAST_MESSAGE_TYPE, {
+    durable: false,
+  });
 
-    channel.assertQueue('', { exclusive: true }, (error2, q) => {
-        if (error2) throw error2;
+  channel.assertQueue('', { exclusive: true }, (error2, q) => {
+    if (error2) throw error2;
 
-        channel.bindQueue(q.queue, 
-                          BROADCAST_MESSAGE_CHANNEL, 
-                          BROADCAST_MESSAGE_TOPIC);
+    channel.bindQueue(
+      q.queue,
+      BROADCAST_MESSAGE_CHANNEL,
+      BROADCAST_MESSAGE_TOPIC
+    );
 
-        channel.consume(q.queue, msg => {
-            const msgToWrite = `${(new Date()).toISOString()} Topic ${msg.fields.routingKey}: ${msg.content.toString()}`;
-            console.log(msgToWrite);
-            fs.appendFile(`${__dirname}/log/obse.log`, `${msgToWrite}${os.EOL}`, { 'flag': 'a' }, (err) => {
-                if (err) throw err;
-                console.log('Saved!');
-            });
-        }, { noAck: true });
-    });
+    channel.consume(
+      q.queue,
+      (msg) => {
+        const msgToWrite = `${new Date().toISOString()} Topic ${
+          msg.fields.routingKey
+        }: ${msg.content.toString()}`;
+        console.log(msgToWrite);
+        fs.appendFile(
+          `${__dirname}/log/obse.log`,
+          `${msgToWrite}${os.EOL}`,
+          { flag: 'a' },
+          (err) => {
+            if (err) throw err;
+            console.log('Saved!');
+          }
+        );
+      },
+      { noAck: true }
+    );
+  });
 };
 
 const subscribeToShutdownChannel = (channel, connection) => {
-    channel.assertExchange(STATE_UPDATE_CHANNEL, 
-                           STATE_UPDATE_TYPE, 
-                           { durable: false });
+  channel.assertExchange(STATE_UPDATE_CHANNEL, STATE_UPDATE_TYPE, {
+    durable: false,
+  });
 
-    channel.assertQueue('', { exclusive: true }, (error2, q) => {
-        if (error2) {
-            throw error2;
+  channel.assertQueue('', { exclusive: true }, (error2, q) => {
+    if (error2) {
+      throw error2;
+    }
+
+    console.log(`[*] Waiting for messages in ${q.queue}`);
+    channel.bindQueue(q.queue, STATE_UPDATE_CHANNEL, '');
+
+    channel.consume(
+      q.queue,
+      (msg) => {
+        if (msg.content) {
+          console.log(`[x] ${msg.content.toString()}`);
+          const newState =
+            msg.content === undefined ? '' : msg.content.toString();
+          if (newState !== STATE_SHUTDOWN) {
+            console.log('Invalid state, skip do nothing!');
+          } else {
+            console.log('shutting down');
+            connection.close();
+            process.exit(0);
+          }
         }
-
-        console.log(`[*] Waiting for messages in ${q.queue}`);
-        channel.bindQueue(q.queue, STATE_UPDATE_CHANNEL, '');
-
-        channel.consume(q.queue, (msg) => {
-            if (msg.content) {
-                console.log(`[x] ${msg.content.toString()}`);
-                const newState = msg.content === undefined ? '' : msg.content.toString();
-                if (newState !== STATE_SHUTDOWN) {
-                    console.log('Invalid state, skip do nothing!');
-                } else {
-                    console.log('shutting down');
-                    connection.close();
-                    process.exit(0);
-                }
-            }
-        }, { noAck: true });
-    });
-}
+      },
+      { noAck: true }
+    );
+  });
+};
 
 module.exports = {
-    listenToTopicAndBroadcast,
-    subscribeToShutdownChannel
-}
+  listenToTopicAndBroadcast,
+  subscribeToShutdownChannel,
+};
--- observer/obse.js
+++ observer/obse.js
@@ -2,15 +2,15 @@ const amqp = require('amqplib/callback_api');
 const obseFunctions = require('./obse-funcs.js');
 
 amqp.connect(process.env.MESSAGE_QUEUE, (error0, connection) => {
-    if (error0) {
-        throw error0;
+  if (error0) {
+    throw error0;
+  }
+  console.log('[*] Connect to rabbitmq from Ovserver');
+  connection.createChannel((error1, channel) => {
+    if (error1) {
+      throw error1;
     }
-    console.log('[*] Connect to rabbitmq from Ovserver')
-    connection.createChannel((error1, channel) => {
-        if (error1) {
-            throw error1;
-        }
-        obseFunctions.listenToTopicAndBroadcast(channel);
-        obseFunctions.subscribeToShutdownChannel(channel, connection);
-    });
+    obseFunctions.listenToTopicAndBroadcast(channel);
+    obseFunctions.subscribeToShutdownChannel(channel, connection);
+  });
 });
